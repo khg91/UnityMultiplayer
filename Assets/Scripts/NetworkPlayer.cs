62089e91f59685388fad1758419294a36c0468c0
khg91 <papaya7456@gmail.com>
2021-12-09 14:50:15 +0900

    로컬플레이어일 때 PlayerControlGUI를 생성

--- Assets/Scripts/NetworkPlayer.cs
+++ Assets/Scripts/NetworkPlayer.cs
@@ -17,15 +17,15 @@ public class NetworkPlayer : NetworkBehaviour
     void Start()
     {
         playerMovement = GetComponent<PlayerMovement>();
+        if (IsLocalPlayer) gameObject.AddComponent<PlayerControlGUI>(); // 로컬 플레이어에게만 GUI 생성
     }
 
     void Update()
     {
-
         if (IsLocalPlayer)  // 로컬 플레이어는 인풋을 통한 Translation 수행 후 RPC 호출
         {
             playerMovement.MoveByInput(moveSpeed);                          // 플레이어 이동 수행
-            playerMovement.RotateByInput(Camera.main);                                 // 플레이어 회전 수행
+            playerMovement.RotateByInput(Camera.main);                      // 플레이어 회전 수행
             SetTransformServerRpc(transform.position, transform.rotation);  // 인풋으로 갱신된 Transform을 RPC로 전달
         }
         else                // 그 외에는 네트워크 멤버를 통한 transform 동기화
@@ -38,14 +38,11 @@ public class NetworkPlayer : NetworkBehaviour
 
 
     /// <summary>
-    /// 네트워크 멤버 동기화 RPC
+    /// 네트워크 멤버 동기화 RPC. 로컬 플레이어로부터 받은 위치, 회전을 네트워크 멤버에 할당한다.
     /// </summary>
-    /// <param name="newPosition">로컬플레이어로부터 받은 새 position</param>
-    /// <param name="newRotation">로컬플레이어로부터 받은 새 rotation</param>
-    /// <param name="rpcParams">RPC 파라미터</param>
 
     [ServerRpc]
-    void SetTransformServerRpc(Vector3 newPosition, Quaternion newRotation, ServerRpcParams rpcParams = default)
+    private void SetTransformServerRpc(Vector3 newPosition, Quaternion newRotation, ServerRpcParams rpcParams = default)
     {
         networkPosition.Value = newPosition;
         networkRotation.Value = newRotation;
